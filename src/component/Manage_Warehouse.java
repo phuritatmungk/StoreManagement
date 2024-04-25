1cb5527d61ac2c01bd65768a084dfc3bbb71909a
aess2 <aess2@montree>
2024-04-26 02:42:41 +0700

    rate

--- src/component/Manage_Warehouse.java
+++ src/component/Manage_Warehouse.java
@@ -27,7 +27,7 @@ public class Manage_Warehouse extends javax.swing.JPanel {
     public Manage_Warehouse() {
         initComponents();
         con = DB.mycon();
-        showProductsInTable();
+        loadAllProducts();
         
             
         TableActionEventEdit event = new TableActionEventEdit() {
@@ -51,6 +51,22 @@ public class Manage_Warehouse extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
+        jPanel1 = new javax.swing.JPanel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel4 = new javax.swing.JLabel();
+        CategoryBox = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jButton5 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        Bgo = new javax.swing.JButton();
+        Bmax = new javax.swing.JTextField();
+        Bmin = new javax.swing.JTextField();
         back_button1 = new javax.swing.JLabel();
         Topic = new javax.swing.JLabel();
         back_button = new javax.swing.JLabel();
@@ -60,6 +76,123 @@ public class Manage_Warehouse extends javax.swing.JPanel {
         jButton2 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable = new javax.swing.JTable();
+
+        jFrame1.setBounds(new java.awt.Rectangle(50, 50, 0, 0));
+        jFrame1.setPreferredSize(new java.awt.Dimension(500, 500));
+        jFrame1.setResizable(false);
+        jFrame1.setSize(new java.awt.Dimension(500, 500));
+        jFrame1.getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 220, 370, 10));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setText("จำนวนคงเหลือ");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 310, -1, -1));
+
+        CategoryBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jPanel1.add(CategoryBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 70, 120, -1));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel2.setText("สืบค้นข้อมูลสินค้าในคลัง");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 20, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setText("เลือกประเภท");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 70, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setText("ช่วงราคาของสินค้า");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 120, -1, -1));
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setText("ราคาของสินค้า");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, -1, -1));
+
+        jButton5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton5.setText("จำนวน : มาก > น้อย");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 340, 150, 30));
+
+        jButton7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton7.setText("จำนวน : น้อย < มาก");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 340, 150, 30));
+
+        jButton8.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton8.setText("ราคา : น้อย < มาก");
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 270, 150, 30));
+
+        jButton9.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton9.setText("ราคา : มาก > น้อย");
+        jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton9ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 270, 150, 30));
+
+        Bgo.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        Bgo.setText("Go");
+        Bgo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BgoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Bgo, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 160, 50, 30));
+
+        Bmax.setText("$Max");
+        Bmax.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                BmaxFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                BmaxFocusLost(evt);
+            }
+        });
+        Bmax.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BmaxActionPerformed(evt);
+            }
+        });
+        Bmax.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                BmaxKeyReleased(evt);
+            }
+        });
+        jPanel1.add(Bmax, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 160, 80, 30));
+
+        Bmin.setText("$Min");
+        Bmin.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                BminFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                BminFocusLost(evt);
+            }
+        });
+        Bmin.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                BminKeyReleased(evt);
+            }
+        });
+        jPanel1.add(Bmin, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 160, 80, 30));
+
+        jFrame1.getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 480, 510));
 
         setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(1280, 720));
@@ -127,6 +260,11 @@ public class Manage_Warehouse extends javax.swing.JPanel {
         jButton2.setBorder(null);
         jButton2.setBorderPainted(false);
         jButton2.setDefaultCapable(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1290, 110, 50, 40));
 
         jTable.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -218,62 +356,145 @@ public class Manage_Warehouse extends javax.swing.JPanel {
         sorter.setRowFilter(RowFilter.regexFilter("(?i)" + search__box.getText().trim(), columnIndexToFilter));
     }//GEN-LAST:event_search__boxKeyReleased
 
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        sortProductsByQuantityDescending();
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+        sortProductsByPriceDescending();
+    }//GEN-LAST:event_jButton9ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        jFrame1.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        sortProductsByPriceAscending();
+    }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        sortProductsByQuantityAscending();
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void BmaxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BmaxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BmaxActionPerformed
+
+    private void BminFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_BminFocusGained
+        if (Bmin.getText().equals("$Min"))
+        {
+            Bmin.setText("");
+            Bmin.setForeground(new Color(0, 0, 0));
+        }
+    }//GEN-LAST:event_BminFocusGained
+
+    private void BminFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_BminFocusLost
+         if (Bmin.getText ().length() ==0){
+            Bmin.setText ("$Min") ;
+            Bmin.setForeground(new Color(0, 0, 0));
+        }
+    }//GEN-LAST:event_BminFocusLost
+
+    private void BmaxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_BmaxFocusGained
+        if (Bmax.getText().equals("$Max"))
+        {
+            Bmax.setText("");
+            Bmax.setForeground(new Color(0, 0, 0));
+        }
+    }//GEN-LAST:event_BmaxFocusGained
+
+    private void BmaxFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_BmaxFocusLost
+         if (Bmax.getText ().length() ==0){
+            Bmax.setText ("$Max") ;
+            Bmax.setForeground(new Color(0, 0, 0));
+        }
+    }//GEN-LAST:event_BmaxFocusLost
+
+    private void BgoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BgoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BgoActionPerformed
+
+    private void BminKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BminKeyReleased
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BminKeyReleased
+
+    private void BmaxKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BmaxKeyReleased
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BmaxKeyReleased
+
+    
+     private void loadAllProducts() {
+        ArrayList<InventoryInfo> productsList = getProductsList(""); // Fetch all products
+        
+        // Update table with all products
+        updateTableWithProducts(productsList);
+    }
+    
+    private void sortProductsByPriceAscending() {
+        ArrayList<InventoryInfo> sortedList = getProductsList("ORDER BY Price ASC");
+        updateTableWithProducts(sortedList);
+    }
+    
+    private void sortProductsByPriceDescending() {
+        ArrayList<InventoryInfo> sortedList = getProductsList("ORDER BY Price DESC");
+        updateTableWithProducts(sortedList);
+    }
+    
+    private void sortProductsByQuantityAscending() {
+        ArrayList<InventoryInfo> sortedList = getProductsList("ORDER BY Quantity ASC");
+        updateTableWithProducts(sortedList);
+    }
+    
+    private void sortProductsByQuantityDescending() {
+        ArrayList<InventoryInfo> sortedList = getProductsList("ORDER BY Quantity DESC");
+        updateTableWithProducts(sortedList);
+    }
     
     
     ArrayList<InventoryInfo> productsArray = new ArrayList<>();
-    
     int position = 0;
-    public ArrayList<InventoryInfo> getProductsList()
-    {
+    private ArrayList<InventoryInfo> getProductsList(String orderBy) {
         ArrayList<InventoryInfo> list = new ArrayList<>();
-        String selectQuery = "SELECT * FROM `inventory`";
-        
-        Statement st;
-        ResultSet rs;
-        
-        try {
-            st = DB.getConnection().createStatement();
-            rs = st.executeQuery(selectQuery);
-            InventoryInfo product;
-            
-            while(rs.next())
-            {
-                product = new InventoryInfo(rs.getInt("No"), rs.getInt("Id"),
-                                      rs.getString("Date"), rs.getString("Name"), rs.getString("Category"),
-                                      rs.getDouble("Cost"), rs.getInt("Quantity"), rs.getDouble("Price"));
+        String selectQuery = "SELECT * FROM `inventory` " + orderBy;
+
+        try (Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(selectQuery)) {
+
+            while (rs.next()) {
+                InventoryInfo product = new InventoryInfo(
+                    rs.getInt("No"), rs.getInt("Id"),
+                    rs.getString("Date"), rs.getString("Name"), rs.getString("Category"),
+                    rs.getDouble("Cost"), rs.getInt("Quantity"), rs.getDouble("Price")
+                );
                 list.add(product);
             }
-            
+
         } catch (SQLException ex) {
             System.out.println(ex);
         }
-        
-        productsArray = list;
+
         return list;
-        
     }
     
-    public void showProductsInTable()
-    {
-        ArrayList<InventoryInfo> productsList = getProductsList();
+    
+    
+    
+    private void updateTableWithProducts(ArrayList<InventoryInfo> productsList) {
         DefaultTableModel model = (DefaultTableModel) jTable.getModel();
-        
         model.setRowCount(0);
-        
-        Object[] row = new Object[7];
-        
-        for(int i = 0; i < productsList.size(); i++)
-        {
-            row[0] = productsList.get(i).getNo();
-            row[1] = productsList.get(i).getId();
-            row[2] = productsList.get(i).getDate();
-            row[3] = productsList.get(i).getName();
-            row[4] = productsList.get(i).getCategory();
-            row[5] = productsList.get(i).getQuantity();
-            row[6] = productsList.get(i).getPrice();
-            
+
+        for (InventoryInfo product : productsList) {
+            Object[] row = {
+                product.getNo(),
+                product.getId(),
+                product.getDate(),
+                product.getName(),
+                product.getCategory(),
+                product.getQuantity(),
+                product.getPrice()
+            };
             model.addRow(row);
-        }      
+        }
     }
     
     public void showProductData(int index)
@@ -290,13 +511,29 @@ public class Manage_Warehouse extends javax.swing.JPanel {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Bgo;
+    private javax.swing.JTextField Bmax;
+    private javax.swing.JTextField Bmin;
+    private javax.swing.JComboBox<String> CategoryBox;
     private javax.swing.JLabel Topic;
     private javax.swing.JLabel back_button;
     private javax.swing.JLabel back_button1;
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton delete_bt;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable jTable;
     private javax.swing.JTextField search__box;
     // End of variables declaration//GEN-END:variables
